e2ea6f91adc468faadae0d68956cc0ab7b4f2444
Sven König <info.micgram.de>
2025-03-20 13:31:30 +0100

    websocket

--- server/main.py
+++ server/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi_socketio import SocketManager
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+socket_manager = SocketManager(app=app)
+
+@app.sio.on("join")
+async def handle_join(sid, *args, **kwargs):
+    await app.sio.emit("test", "123")
 
 # Mount the static directory located inside the server folder
 app.mount("/static", StaticFiles(directory="server/static"), name="static")
@@ -12,7 +18,6 @@ app.mount("/static", StaticFiles(directory="server/static"), name="static")
 async def read_index():
     # Serve the static HTML file from the new location
     return FileResponse("server/static/index.html")
-
 
 if __name__ == "__main__":
     import uvicorn
--- server/static/js/main.js
+++
@@ -1,2 +0,0 @@
-// Update the page content once the JS loads.
-document.getElementById("message").innerText = "Hello World from JavaScript!";
