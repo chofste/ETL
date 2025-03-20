432ef31eb40d2539c03e57aac1b71ca292afee22
Sven König <info.micgram.de>
2025-03-20 13:32:10 +0100

    websockets

--- server/main.py
+++ server/main.py
@@ -1,25 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi_socketio import SocketManager
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-socket_manager = SocketManager(app=app)
+sio = SocketManager(app=app)
 
-@app.sio.on("join")
+app.mount("/", StaticFiles(directory="server/static", html=True), name="static")
+
+@app.sio.on('join')
 async def handle_join(sid, *args, **kwargs):
-    await app.sio.emit("test", "123")
+    await app.sio.emit('lobby', 'User joined')
+    
+@sio.on('test')
+async def test(sid, *args, **kwargs):
+    await sio.emit('hey', 'joe')
 
-# Mount the static directory located inside the server folder
-app.mount("/static", StaticFiles(directory="server/static"), name="static")
+#@socket_manager.on("connect")
+#async def connect(sid, env):
+#    print("New Client Connected to This id :"+" "+str(sid))
+    
+#@socket_manager.on("disconnect")
+#async def disconnect(sid):
+#    print("Client Disconnected: "+" "+str(sid))
+    
 
 
-@app.get("/")
-async def read_index():
-    # Serve the static HTML file from the new location
-    return FileResponse("server/static/index.html")
+#app.websocket("/ws")
+#async def websocket_endpoint(websocket: WebSocket):
+#    await websocket.accept()
+#    while True:
+#        data = await websocket.receive_text()
+#        await websocket.send_text(f"Message text was: {data}")
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ server/static/index.js
@@ -0,0 +1,8 @@
+/*var socket = io("http://localhost/ws/socket.io");
+
+socket.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});*/
